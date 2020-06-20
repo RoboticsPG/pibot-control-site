d6b0bac97cea00a1fe97dc6a2e3a3dd32079626a
Harrison Asmar <harrisonasmar@gmail.com>
2020-06-20 15:56:53 +1000

    Lots of changes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var speed = 0
 var forward = true
 var left = false
 var turn_speed = 0
+var direction = forward
 
 var slider_speed = document.getElementById("speed_slide");
 
@@ -14,8 +15,22 @@ function show_video() {
     document.getElementById('camera_feed').style.maxHeight = "640px";
 }
 
+function send_commands() {
+    if (speed > 0) {
+        direction = 'forward';
+    } else if (speed = 0){
+        direction = 'stop';
+    } else if (speed < 0) {
+        direction = 'backward';
+        speed = speed * -1
+    }
+    
+    $("#result").load("http://" + bot + ":8090/post/"+direction+"/" + speed + "/" + duration);
+}
+
 slider_speed.oninput = function() {
     speed.innerHTML = this.value;
+    document.getElementById('speedReadout').value=this.value;
     console.log("Speed: "+speed)
 }
 //https://www.w3schools.com/howto/howto_js_rangeslider.asp
