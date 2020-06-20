9102cf3904a8899544f183bf5e4a9fadc980166a
Harrison Asmar <harrisonasmar@gmail.com>
2020-06-20 17:53:11 +1000

    Should be working now

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 console.log('javascript active')
 
-var speed = 0
-var forward = true
-var left = false
-//var turn_speed = 0
-var direction = forward
+//var speed = document.getElementById('speedReadout').value
+var turn_speed = 0
+var direction = 'forward'
 var duration = 1 //static but can be changed for debugging
 var interval
 
 var slider_speed = document.getElementById("speed_slide");
 var turn_speed = document.getElementById("turn_slide");
 
-turn_speed.innerHTML = 0
+turn_speed.innerHTML
 
 function show_video() {
     console.log('video active');
@@ -21,36 +19,35 @@ function show_video() {
 }
 
 function send_commands() {
-    console.log(turn_speed.value) //used for debugging
-    if (speed = 0){
+    console.log(turn_speed.value); //used for debugging
+    speed = document.getElementById('speedReadout').value
+    if (document.getElementById('speedReadout').value == 0){
         direction = 'stop';
-        speed = 0;
-        duration = 0;
+        //speed = 0;
+        //duration = 0;
     } 
-    if (turn_speed.value = -1){
+    if (document.getElementById('speedReadout').value > 0) {
+        direction = 'forward';
+    } else if (document.getElementById('speedReadout').value < 0) {
+        direction = 'backward';
+        speed = document.getElementById('speedReadout').value*-1;
+    } if (turn_speed.value == -1){
         direction = 'left';
-    } else if (turn_speed.value = 1) {
+    } else if (turn_speed.value == 1) {
         direction = 'right';
-    } else {
-        if (speed > 0) {
-            direction = 'forward';
-        } else if (speed < 0) {
-            direction = 'backward';
-            speed = speed * -1
-        }
-    }
+    } 
     var bot = document.getElementById('bot-id').value
     $("#result").load("http://" + bot + ":8090/post/"+direction+"/" + speed + "/" + duration);
     console.log("http://" + bot + ":8090/post/"+direction+"/" + speed + "/" + duration)
 }
 
 slider_speed.oninput = function() {
-    speed.innerHTML = this.value;
+    var speed = this.value;
     document.getElementById('speedReadout').value=this.value;
     console.log("Speed: "+document.getElementById('speedReadout').value)
 }
 turn_speed.oninput = function() {
-    turn_speed.value = this.value;
+    turn_speed.innerHTML = this.value;
     console.log(turn_speed.value) //used for debugging
     if (turn_speed.value < 0) {
         document.getElementById('angleReadout').value = "Left";
@@ -59,7 +56,7 @@ turn_speed.oninput = function() {
     } else {
         document.getElementById('angleReadout').value = "Straight";
     }
-    console.log("Turn direction: "+document.getElementById('angleReadout').value);
+    console.log("Turn direction: "+/*document.getElementById('angleReadout').value*/turn_speed.innerHTML);
 }
 function start_commands() {
     interval=setInterval(send_commands,1000);
