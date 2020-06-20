0e967c411bdb2c9995352ed0c958b833be75f323
Harrison Asmar <harrisonasmar@gmail.com>
2020-06-20 18:11:19 +1000

    Added comments to js to make it more readable

--- script.js
+++ script.js
@@ -1,58 +1,56 @@
-console.log('javascript active')
+console.log('javascript active') //makes sure javascript is working
 
-//var speed = document.getElementById('speedReadout').value
+//sets variables that need to be global
 var turn_speed = 0
 var direction = 'forward'
 var duration = 1 //static but can be changed for debugging
 var interval
 
-var slider_speed = document.getElementById("speed_slide");
-var turn_speed = document.getElementById("turn_slide");
-
-turn_speed.innerHTML
+var slider_speed = document.getElementById("speed_slide"); //imports the speed slider
+var turn_speed = document.getElementById("turn_slide"); //imports the turn slider
 
 function show_video() {
-    console.log('video active');
-    var bot = document.getElementById("bot-id").value;
-    document.getElementById('camera_feed').innerHTML= '<img src="http://'+bot+':8080/stream/video.mjpeg" alt="camera_feed">';
-    document.getElementById('camera_feed').style.maxHeight = "640px";
+    console.log('video active'); //debugging message
+    var bot = document.getElementById("bot-id").value; //gets the bot IP address for the video
+    document.getElementById('camera_feed').innerHTML= '<img src="http://'+bot+':8080/stream/video.mjpeg" alt="camera_feed">'; //initiaises camera feed
+    document.getElementById('camera_feed').style.maxHeight = "640px"; //sets max size so it doesn't take over the page
 }
 
-function send_commands() {
+function send_commands() { //runs every second when the interval is enabled
     console.log(turn_speed.value); //used for debugging
-    speed = document.getElementById('speedReadout').value
+    speed = document.getElementById('speedReadout').value //gets the current value from the speed slider
     if (document.getElementById('speedReadout').value == 0){
         direction = 'stop';
         //speed = 0;
         //duration = 0;
     } 
-    if (document.getElementById('speedReadout').value > 0) {
+    if (document.getElementById('speedReadout').value > 0) { //if the speed is positive
         direction = 'forward';
-    } else if (document.getElementById('speedReadout').value < 0) {
+    } else if (document.getElementById('speedReadout').value < 0) { //if the speed is negative
         direction = 'backward';
-        speed = document.getElementById('speedReadout').value*-1;
-    } if (turn_speed.value == -1){
-        if (direction == 'forward') {
-            direction = 'left';
+        speed = document.getElementById('speedReadout').value*-1; //make negative speed positive so the robot can process it
+    } if (turn_speed.value == -1){ //if turning left
+        if (direction == 'forward') { //if positive speed
+            direction = 'left'; //go the selected direction
         } else {
-            direction = 'right';
+            direction = 'right'; //if negative speed, go the opposite direction
         }
-    } else if (turn_speed.value == 1) {
-        if (direction == 'forward') {
+    } else if (turn_speed.value == 1) { //if turning right
+        if (direction == 'forward') { //same as before
             direction = 'right';
         } else {
             direction = 'left';
         }
     } 
-    var bot = document.getElementById('bot-id').value
-    $("#result").load("http://" + bot + ":8090/post/"+direction+"/" + speed + "/" + duration);
-    console.log("http://" + bot + ":8090/post/"+direction+"/" + speed + "/" + duration)
+    var bot = document.getElementById('bot-id').value //gets the bot IP address from the text box
+    $("#result").load("http://" + bot + ":8090/post/"+direction+"/" + speed + "/" + duration); //background process to send commands to bot
+    console.log("http://" + bot + ":8090/post/"+direction+"/" + speed + "/" + duration) //sends url command to console for debugging
 }
 
-slider_speed.oninput = function() {
-    var speed = this.value;
-    document.getElementById('speedReadout').value=this.value;
-    console.log("Speed: "+document.getElementById('speedReadout').value)
+slider_speed.oninput = function() { //runs when speed slider value is changed/ when it is slid
+    var speed = this.value; //sets the speed variable
+    document.getElementById('speedReadout').value=this.value; //changes the readout to show the new value
+    console.log("Speed: "+document.getElementById('speedReadout').value) //debuggingn loggingw
 }
 turn_speed.oninput = function() {
     turn_speed.innerHTML = this.value;
@@ -66,11 +64,11 @@ turn_speed.oninput = function() {
     }
     console.log("Turn direction: "+/*document.getElementById('angleReadout').value*/turn_speed.innerHTML);
 }
-function start_commands() {
-    interval=setInterval(send_commands,1000);
+function start_commands() { //function to start sending commands every second
+    interval=setInterval(send_commands,1000); //sets interval to send command every second
 }
 function stop_commands() {
-    clearInterval(interval);
+    clearInterval(interval); //stops the sending of commands
 }
 /* Links:
 https://www.w3schools.com/howto/howto_js_rangeslider.asp
